65440723953cf7c643030d3be846c27ca8bbffb8
Viktor Sorokin <jabervik@gmail.com>
2025-02-09 12:00:22 +0300

    !!234

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Tes4124124t')
+    print_hi('Tes4123334124t')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
